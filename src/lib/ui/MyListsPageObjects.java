1ce3667b8a154ff86015639af7002fd18e71b759
Shelkovica <senvica@mail.ru>
2022-01-14 11:57:00 +0400

    Ex 10: Настройка работы на iOS

--- src/lib/ui/MyListsPageObjects.java
+++ src/lib/ui/MyListsPageObjects.java
@@ -1,14 +1,13 @@
 package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
 
 public class MyListsPageObjects extends MainPageObject
 {
 
     public static final String
-        FOLDER_BY_NAME_TPL = "//*[@text='{FOLDER_NAME}']",
-        ARTICLE_BY_TITLE_TPL = "//*[@text='{TITLE}']";
+        FOLDER_BY_NAME_TPL = "xpath://*[@text='{FOLDER_NAME}']",
+        ARTICLE_BY_TITLE_TPL = "xpath://*[@text='{TITLE}']";
 
     private static String getFolderXpathByName(String name_of_folder)
     {
@@ -29,7 +28,7 @@ public class MyListsPageObjects extends MainPageObject
     {
         String folder_name_xpath = getFolderXpathByName(name_of_folder);
         this.waitForElementAndClick(
-                By.xpath(folder_name_xpath),
+                folder_name_xpath,
                 "Cannot find folder by name "+name_of_folder,
                 5
         );
@@ -40,7 +39,7 @@ public class MyListsPageObjects extends MainPageObject
     {
         String article_xpath = getSavedArticleXpathByTitle(article_title);
         this.waitForElementPresent(
-                By.xpath(article_xpath),
+                article_xpath,
                 "CAnnot find saved article by title "+article_title,
                 15
         );
@@ -50,7 +49,7 @@ public class MyListsPageObjects extends MainPageObject
     {
         String article_xpath = getSavedArticleXpathByTitle(article_title);
         this.waitForElementNotPresent(
-                By.xpath(article_xpath),
+                article_xpath,
                 "Saved article still present with title "+article_title,
                 15
         );
@@ -61,12 +60,10 @@ public class MyListsPageObjects extends MainPageObject
         this.waitForArticleToAppearByTitle(article_title);
         String article_xpath = getSavedArticleXpathByTitle(article_title);
         this.swipeElementToLeft(
-                By.xpath(article_xpath),
+                article_xpath,
                 "Cannon find saved article"
         );
 
         this.waitForArticleToDisappearByTitle(article_title);
     }
-
-
 }
